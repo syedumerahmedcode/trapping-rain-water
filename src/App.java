a5f5c2b4422f3a8a88a9917f8e3661ce66638e21
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-12-14 19:04:30 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /**
+         * Given 'n' non-negative integers representing an elevation map where the width
+         * of each bar is 1, compute how much water it can trap after a rainfall.
+         * 
+         * input:{0,1,0,2,1,0,1,3,2,1,2,1}
+         * output: 6
+         * Explanation: The above elevation map (black section in the leetcode question)
+         * is represented by array {0,1,0,2,1,0,1,3,2,1,2,1}. In this case, 6 unit of
+         * rain water(blue section in the leetcode question) are being trapped.
+         */
+        System.out.println("Hello, World!");
+    }
+}
